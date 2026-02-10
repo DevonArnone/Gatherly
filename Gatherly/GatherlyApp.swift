f70ce5a7e7b2a4a033e831939fc670ace84c0165
Devon Arnone <87498263+DevonArnone@users.noreply.github.com>
2026-02-10 12:56:29 -0500

    Initial commit

---
+++ Gatherly/GatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherlyApp.swift
+//  Gatherly
+//
+//  Created by Devon Arnone on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct GatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
